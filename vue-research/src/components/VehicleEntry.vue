9363e180a315b3d4f40e9e0530dd95cbfaa3eca2
Thegodfather504 <luciano.miserendino70@gmail.com>
2023-12-19 16:14:24 +0000

    Added nemw page to route to Startes to create component to accept vehicle ticket information

---
+++ vue-research/src/components/VehicleEntry.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <!-- TODO lets get the other two dropdowns 
+            before we tackle loading data into the controls from the back end 
+        -->
+        <form>
+            <select name="Year">
+                <option value="2023">2023</option>
+                <option value="2022">2022</option>
+                <option value="2021">2021</option>
+                <option value="2020">2020</option>
+            </select>
+            <select name="Make">
+                <option value="Chevrolet">Chevrolet</option>
+                <option value="Dodge">Dodge</option>
+                <option value="Ford">Ford</option>
+                <option value="Nissan">Nissan</option>
+            </select>
+            <!-- this select is dependent on the make -->
+            <!--  Lets have this control appear after a yeat and make is eslected -->
+            <select name="Model">
+                <option value="2023">2023</option>
+                <option value="2022">2022</option>
+                <option value="2021">2021</option>
+                <option value="2020">2020</option>
+            </select>
+            <input type="submit" value="Submit"/>
+        </form>
+    </div>
+</template>
---
+++ vue-research/src/views/TicketView.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import VehicleEntry from '@/components/VehicleEntry.vue';
+</script>
+
+
+<template>
+    <div class="submission">
+        <h1> OnCall Ticket Submission </h1>
+        <VehicleEntry />
+
+    </div>
+   
+</template>
+
+<style>
+
+@media (min-width: 1024px){
+    .submission {
+        min-height: 100vh;
+        display: flex;
+        align-items: center;
+    }
+}
+</style>
